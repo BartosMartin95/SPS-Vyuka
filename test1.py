d1cc50a5e930f089012567ba0fb592fb076b5e82
Martin Bartoš <martin.bartos971014@gamil.com>
2023-08-22 18:52:18 +0200

    Second commit

--- test1.py
+++ test1.py
@@ -2,5 +2,18 @@ a = 5
 b = 3
 while b < 7:
     a += 1
-    print(a)
+    #print(a)
     b+=1
+
+print(a)
+print(b)
+
+
+def sum_of_two_numbers(a, b):
+    c = a + b
+    return c
+
+
+d = sum_of_two_numbers(a, b)
+print(d)
+
