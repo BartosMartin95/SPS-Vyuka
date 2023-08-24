1c95f066c4cd7b4a03fb0ce9fd4208375a23116f
MartinBartos97 <martin.bartos971014@gmail.com>
2023-08-24 08:44:18 +0200

    Tuples and lists runtime 1.

--- test1.py
+++ test1.py
@@ -1,19 +1,39 @@
-a = 5
-b = 3
-while b < 7:
+import time
+
+a = 1
+list1 = []
+
+while a < 10000:
+    list1.append(a)
     a += 1
-    #print(a)
-    b+=1
 
-print(a)
-print(b)
+start_time_tuple = time.time()
+
+tuple1 = (0, )
+
+b = 1
+
+while b < 10000:
+    tuple2 = (b, )
+    tuple1 += tuple2
+    b += 1
+
+s1 = time.process_time()
+for x in list1:
+    print(x)
+e1 = time.process_time()
+
+s2 = time.process_time()
+for x in tuple1:
+    print(x)
+e2 = time.process_time()
+
+run_time1 = e1-s1
+run_time2 = e2-s2
 
 
-def sum_of_two_numbers(a, b):
-    c = a + b
-    return c
+print("runtime list", run_time1)
+print("runtime tuple", run_time2)
 
 
-d = sum_of_two_numbers(a, b)
-print(d)
 
---
+++ test2.py
@@ -0,0 +1,10 @@
+tuple1 = ("a", "b", "c", "d", "e", "a", "b")
+e = tuple1.count("a")
+d = tuple1.index("b")
+print(d)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(len(set3))
